673790c533f690092a518f0306cea90d6caf9028
Leila Weiss <leila.weiss@powerhrg.com>
2019-03-21 08:34:52 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name
+    @name
+  end
+  def name=(person_name)
+    @name = person_name
+  end
+  def job
+    @job
+  end
+  def job=(new_job)
+    @job = new_job
+  end
+end
